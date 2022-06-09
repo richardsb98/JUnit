221603627e541faf1b9fe0f341bd973362683205
Richard <richard281298@hotmail.co.uk>
2022-06-09 08:51:24 +0100

    first commit

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,8 @@
+
+public class SimpleCalculator {
+
+    public int minus (int x, int y){
+        return x - y;
+    }
+
+}
---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCalculatorTest  {
+
+    @Test
+    void fiveminusfourEqualone(){
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        Assertions.assertEquals(1,calculator.minus(5,4), "error with computation");
+
+    }
+
+}
